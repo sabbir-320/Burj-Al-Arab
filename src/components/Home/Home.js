edbed0926de8b6692fa635d29290c732d4555b64
sabbir-320 <sabbir195661@gmail.com>
2020-11-28 22:52:44 +0600

    sime server functionality added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Room from './RoomCom/Room';
+
+const Home = () => {
+    
+    return (
+        <div>
+            <Room></Room>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAX362yjrnq40PT4lfXeTHN7Q0O1Y2LH0s",
+  authDomain: "burj-al-arab-8e03f.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-8e03f.firebaseio.com",
+  projectId: "burj-al-arab-8e03f",
+  storageBucket: "burj-al-arab-8e03f.appspot.com",
+  messagingSenderId: "661181212935",
+  appId: "1:661181212935:web:742f5a6609a24fff24bda2"
+};
+  export default firebaseConfig;
